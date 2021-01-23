a1bdfc5055e909c9354dc41b5937c970ef289785
yuli <275452142@qq.com>
2021-01-23 17:41:52 +0800

    1、stream完善

--- src/main/scala/com/yuli/functional/chapter5/Stream.scala
+++ src/main/scala/com/yuli/functional/chapter5/Stream.scala
@@ -1,5 +1,7 @@
 package com.yuli.functional.chapter5
 
+import com.yuli.functional.chapter5.Stream.{cons, empty, unfold}
+
 import scala.annotation.tailrec
 
 /**
@@ -66,16 +68,15 @@ sealed trait Stream[+A] {
   }
 
   /**
-   * 练习5.3返回满足断言的函数
+   * 练习5.3返回连续满足断言的函数，遇到不满足就中断
    *
    * @param p
    * @return
    */
   def takeWhile(p: A => Boolean): Stream[A] = {
     def loop(stream: Stream[A], newStream: Stream[A]): Stream[A] = stream match {
-      case Empty => newStream
+      case _ => newStream
       case Cons(h, l) if p(h()) => Stream.cons(h(), loop(l(), newStream))
-      case Cons(_, l) => loop(l(), newStream)
     }
 
     loop(this, Empty)
@@ -118,10 +119,117 @@ sealed trait Stream[+A] {
    * 练习5.4
    * 检查Stream中所有元素是否与给定的断言匹配。如果不匹配就直接返回。
    * 因为如果不匹配需要立即返回，所有b不能被求值，需要在&&之前就返回
+   *
    * @param p
    * @return
    */
   def forAll(p: A => Boolean): Boolean = foldRight(true)((a, b) => p(a) && b)
+
+  /**
+   * 练习5.5
+   *
+   * @param p
+   * @return
+   */
+  def takeWhileFold(p: A => Boolean): Stream[A] = foldRight(Empty: Stream[A])((a, b) => if (p(a)) Stream.cons(a, b) else Empty)
+
+  /**
+   * 练习5.6
+   *
+   * @return
+   */
+  def headOptionFold: Option[A] = foldRight(None: Option[A])((a, _) => if (a == Empty) None else Some(a))
+
+  /**
+   * 练习5.7
+   * 用rightFold实现map
+   *
+   * @param f
+   * @tparam B
+   * @return
+   */
+  def map[B](f: A => B): Stream[B] = foldRight(Empty: Stream[B])((a, b) => Stream.cons(f(a), b))
+
+  def filter(p: A => Boolean): Stream[A] = foldRight(Empty: Stream[A])((a, b) => if (p(a)) Stream.cons(a, b) else b)
+
+  def append[B >: A](s: => Stream[B]): Stream[B] = foldRight(s)((a, b) => Stream.cons(a, b))
+
+  def flatMap[B](f: A => Stream[B]): Stream[B] = foldRight(Empty: Stream[B])((a, b) => f(a) append b)
+
+  /**
+   * 因为是惰性求值,所有不会对匹配到后的Stream求值.
+   *
+   * @param p
+   * @return
+   */
+  def find(p: A => Boolean): Option[A] = filter(p).headOptionFold
+
+  /**
+   * 练习5.13
+   *
+   * @param f
+   * @tparam B
+   * @return
+   */
+  def mapViaUnfold[B](f: A => B): Stream[B] = Stream.unfold(this) {
+    case Empty => None
+    case Cons(h, t) => Some(f(h()), t())
+  }
+
+  def takeViaUnfold(n: Int): Stream[A] = Stream.unfold((n, this)) {
+    case (n, Cons(h, t)) if n > 0 => Some(h(), (n - 1, t()))
+    case (_, Empty) => None
+    case (0, Cons(h, t)) => None
+  }
+
+  def takeWhileViaUnfold(p: A => Boolean): Stream[A] = Stream.unfold(this) {
+    case Cons(h, t) if p(h()) => Some(h(), t())
+    case _ => None
+  }
+
+  def zipWith[B, C](s: Stream[B])(f: (A, B) => C): Stream[C] = unfold((this, s)) {
+    case (Cons(h1, t1), Cons(h2, t2)) => Some((f(h1(), h2()), (t1(), t2())))
+    case _ => None
+  }
+
+  def zipAll[B](s: Stream[B]): Stream[(Option[A], Option[B])] = unfold((this, s)) {
+    case (Cons(h1, t1), Cons(h2, t2)) => Some((Some(h1()), Some(h2())), (t1(), t2()))
+    case (Empty, Cons(h2, t2)) => Some((None, Some(h2())), (Empty, t2()))
+    case (Cons(h1, t1), Empty) => Some((Some(h1()), None), (t1(), Empty))
+    case (Empty, Empty) => None
+  }
+
+  /**
+   * 练习5.14
+   * 检查一个stream是否是另一个stream的前缀
+   *
+   * @param s
+   * @tparam A
+   * @return
+   */
+  def startsWith[A](s: Stream[A]): Boolean = zipAll(s).takeWhileViaUnfold(!_._2.isEmpty).forAll {
+    case (h1, h2) => h1 == h2
+  }
+
+  /**
+   * 练习5.15
+   * 用unfold实现tails，返回所有后缀
+   *
+   * @return
+   */
+  def tails: Stream[Stream[A]] = unfold(this) {
+    case s@Cons(_, t) => Some(s, t())
+    case _ => None
+  }.append(Empty)
+
+  /**
+   * 是否包含子序列
+   *
+   * @param s
+   * @tparam A
+   * @return
+   */
+  def hasSubsequence[A](s: Stream[A]): Boolean = tails exists (_ startsWith s)
 
 }
 
@@ -136,6 +244,67 @@ object Stream {
     lazy val tail = tl
     Cons(() => head, () => tail)
   }
+
+  /**
+   * 练习5.8
+   * 根据给定值返回一个无限流
+   *
+   * @param a
+   * @tparam A
+   * @return
+   */
+  def constant[A](a: A): Stream[A] = {
+    lazy val ones: Stream[A] = cons(a, ones)
+    ones
+  }
+
+  /**
+   * 练习5.9
+   *
+   * @param n
+   * @return
+   */
+  def from(n: Int): Stream[Int] = cons(n, from(n + 1))
+
+  /**
+   * 练习5.10
+   * 生成斐波那契数列
+   *
+   * @return
+   */
+  def fibs: Stream[Int] = {
+    def loop(n1: Int, n2: Int): Stream[Int] = {
+      cons(n1, loop(n2, n1 + n2))
+    }
+
+    loop(0, 1)
+  }
+
+  /**
+   * 练习5.11
+   * 更加通用的构造流函数，它接受一个初始状态，以及一个在生成的Stream中用于产生下一个状态和下一个值的函数
+   *
+   * @param z
+   * @param f
+   * @tparam A
+   * @tparam S
+   * @return
+   */
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = f(z) match {
+    case None => Empty
+    case Some((a, s)) => cons(a, unfold(s)(f))
+  }
+
+  /**
+   * 练习5.13通过unfold实现fibs,from,constant
+   *
+   * @return
+   */
+  def fibsUnfold: Stream[Int] = unfold((0, 1))(a => Some(a._1, (a._2, a._1 + a._2)))
+
+  def constantUnfold(n: Int): Stream[Int] = unfold(n)(s => Some(s, s))
+
+  def fromUnfold(n: Int): Stream[Int] = unfold(n)(s => Some(s + 1, s))
 
   def empty[A](): Stream[A] = Empty
 
--- src/main/scala/com/yuli/functional/chapter5/StreamTest.scala
+++ src/main/scala/com/yuli/functional/chapter5/StreamTest.scala
@@ -8,21 +8,62 @@ package com.yuli.functional.chapter5
  */
 object StreamTest extends App {
   private val stream: Stream[Int] = Stream(1, 2, 3, 4, 5)
+//  /**
+//   * 练习5.1
+//   */
+//  println(stream.toList)
+//  /**
+//   * 练习5.2
+//   */
+//  println(stream.take(2).toList)
+//  println(stream.drop(3).toList)
+//  /**
+//   * 练习5.3
+//   */
+//  println(stream.takeWhile(p => p % 2 == 0).toList)
+//  /**
+//   * 练习5.4
+//   */
+//  println(stream.forAll(p => p.isFinite))
+//  /**
+//   * 练习5.5
+//   */
+//  println(stream.takeWhileFold(p => p % 2 == 0).toList)
+//  /**
+//   * 练习5.6
+//   */
+//  println(stream.headOptionFold)
+//  /**
+//   * 练习5.7
+//   */
+//  println(stream.append(Stream.apply(6)).toList)
+//  println(stream.map(_ + 1).toList)
+//
+//  stream.map(_ + 10).filter(_ % 2 == 0).toList
+//  /**
+//   * 无限流，使用惰性求值可以避免中间运算,就不会产生死循环
+//   */
+//  val ones: Stream[Int] = Stream.cons(1, ones);
+//  println(ones.take(5).toList)
+//  println(ones.exists(_ % 2 != 0))
+//  println(ones.map(_ + 1).exists(_ % 2 == 0))
+//  println(Stream.from(1).take(5).toList)
+//  println(Stream.fibs.take(8).toList)
+//  println(Stream.fibsUnfold.take(8).toList)
+//  println(Stream.constantUnfold(1).take(5).toList)
+//  println(Stream.from(1).take(5).toList)
+//
+//  /**
+//   * 练习5.13
+//   */
+//  println(stream.mapViaUnfold(_ + 1).toList)
+//  println(stream.takeViaUnfold(3).toList)
+//  println(stream.takeWhileViaUnfold(_ % 2 == 0).toList)
+//
+//  private val stream2: Stream[Int] = Stream(1, 2, 3,5)
+//  println(stream.startsWith(stream2))
   /**
-   * 练习5.1
+   * 练习5.15
    */
-  println(stream.toList)
-  /**
-   * 练习5.2
-   */
-  println(stream.take(2).toList)
-  println(stream.drop(3).toList)
-  /**
-   * 练习5.3
-   */
-  println(stream.takeWhile(p => p % 2 == 0).toList)
-  /**
-   * 练习5.4
-   */
-  println(stream.forAll(p => p.isFinite))
+  stream.tails.toList.foreach(f => println(f.toList))
 }
